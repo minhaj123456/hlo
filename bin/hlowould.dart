8e70aa5c14bd8430e2d1c7f3a30a46ada524a5db
minhaj123456 <133559085+minhaj123456@users.noreply.github.com>
2023-08-23 14:09:30 +0530

    ert

---
+++ bin/hlowould.dart
@@ -0,0 +1,5 @@
+import 'package:hlowould/hlowould.dart' as hlowould;
+
+void main(List<String> arguments) {
+  print('Hello world: ${hlowould.calculate()}!');
+}
---
+++ test/hlowould_test.dart
@@ -0,0 +1,8 @@
+import 'package:hlowould/hlowould.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
